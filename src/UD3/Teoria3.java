16f7e8c8b4087ace5ce5d27b984b3aa66b55d509
IESPIRINGALLA\lopparpab <lopparpab - Aula>
2024-11-15 19:53:26 +0100

    Clase 24/11/15 Inicio Teoría 3

---
+++ src/UD3/Teoria3.java
@@ -0,0 +1,46 @@
+package src.UD3;
+
+public class Teoria3 {
+    //Viernes 15/11/2024
+    private static void imprimeTablaMultiplicar(int valor) {
+        System.out.println("Tabla de multiplicar del " + valor);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(valor + " * " + i + " = " + valor * i);
+        }
+        /*
+         * Código que teníamos en el main
+         *
+         * for (int valor = 1; valor <= 2; valor++) {
+         *    imprimeTablaMultiplicar(valor);
+         * }
+         */
+    }
+
+    private static int sumar(int numero1, int numero2) {
+        //int resultado = numero1 + numero2;
+        return numero1 + numero2;    //Siempre que el método no sea void debe tener return
+    }
+
+    private static double hacerMedia(int numero1, int numero2){
+        return (double) (numero1 + numero2) /2;
+    }
+
+    private static void hacerPresentacion(){
+        System.out.println("Pablo, edad 27, Vegadeo");;
+    }
+
+    private static void imprimirPresentacion(String nombre, String lugarNacimiento, int edad){
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(lugarNacimiento);
+    }
+
+    public static void main(String[] args) {
+        //Cualquier código que queramos utilizar debemos llamarlo desde el método main
+        System.out.println(sumar(10, 10));
+        System.out.println(hacerMedia(9, 8));
+        hacerPresentacion();
+        imprimirPresentacion("Pablo", "Vegadeo", 27);
+
+    }
+}
