30b8fe8b2f9a11b4570f65db0818b5579b78cd35
IESPIRINGALLA\lopparpab - 103 <lopparpab>
2025-01-13 18:45:25 +0100

    Clase 25/01/13 Teoria4

---
+++ src/UD4/Semaforo.java
@@ -0,0 +1,5 @@
+package src.UD4;
+
+public enum Semaforo {
+    ROJO, AMBAR, VERDE
+}
---
+++ src/UD4/Teoria4.java
@@ -0,0 +1,7 @@
+package src.UD4;
+
+public class Teoria4 {
+    public static void main(String[] args) {
+        System.out.println(Semaforo.AMBAR);
+    }
+}
