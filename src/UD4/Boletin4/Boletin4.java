0ea0a83f1b584195125ec53fde52aca7c955f251
IESPIRINGALLA\lopparpab - 103 <lopparpab>
2025-03-06 20:04:51 +0100

    Clase 25/03/06 Boletin 4

---
+++ src/UD4/Boletin4/Boletin4.java
@@ -0,0 +1,9 @@
+package src.UD4.Boletin4;
+
+import java.util.Scanner;
+
+public class Boletin4 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/UD4/Boletin4/Vectores/Ejercicio2.java
@@ -0,0 +1,34 @@
+package src.UD4.Boletin4.Vectores;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Escribir una aplicación que rellene un vector de enteros y visualice los valores\n" +
+                "superiores a la media.\n");
+        System.out.println("Ingresa el tamaño del vector:");
+        Scanner entrada = new Scanner(System.in);
+        int tamannoVector = entrada.nextInt();
+
+        int [] vectorEnteros = new int [tamannoVector];
+
+        for (int i = 0; i < vectorEnteros.length; i++) {
+            vectorEnteros[i]= (int) (100 * Math.random());
+        }
+
+        double valorMedio = 0;
+
+        for (int i = 0; i < vectorEnteros.length; i++){
+            valorMedio += vectorEnteros[i];
+        }
+        valorMedio = valorMedio / tamannoVector;
+
+        for (int i = 0; i < vectorEnteros.length; i++){
+            if (vectorEnteros[i]>valorMedio){
+                System.out.print("\033[32m"+vectorEnteros[i]+" ");
+            } else {
+                System.out.print("\033[31m"+vectorEnteros[i]+" ");
+            }
+        }
+    }
+}
