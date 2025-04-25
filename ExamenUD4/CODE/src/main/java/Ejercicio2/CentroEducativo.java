5f27ce942bb6505540ade0777ee51660f9a14901
IESPIRINGALLA\lopparpab - 103 <lopparpab>
2025-04-25 21:13:03 +0200

    Examen UD4 Pequeño comentario

--- ExamenUD4/CODE/src/main/java/Ejercicio2/CentroEducativo.java
+++ ExamenUD4/CODE/src/main/java/Ejercicio2/CentroEducativo.java
@@ -6,6 +6,11 @@ import java.util.HashMap;
 public class CentroEducativo extends EdificioPublico{
     private EtapaEducativa etapa;
     private HashMap<String, ArrayList<Estudiante>> curso;
+    /*
+    Habiendo N cursos únicos posibles la mejor opción es utilizar un HashMap con la clave con el nombre del curso y
+    un ArrayList<Estudiante> donde añadir a los alumnos matriculados
+
+     */
 
     public CentroEducativo() {
     }
